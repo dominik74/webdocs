0d8211504e717e188d3a889c47ec104fd03f2a6d
dominik74 <60546513+dominik74@users.noreply.github.com>
2022-03-06 14:31:08 +0100

    images now open with sidebar visible & code cleanups

--- src/main.cpp
+++ src/main.cpp
@@ -10,14 +10,13 @@
 namespace fs = filesystem;
 namespace strex = stringExtensions;
 
-void generateProjectFolders();
+void generateProjectFiles();
 void build();
 void loadConfig();
 std::string configGetLineValue(const std::vector<std::string>& lines, int index);
 void clearBuildDir();
-std::string generateNavCode();
-std::string buildPage(const std::string& contents);
-void generateIndexFile();
+void generateNavCode();
+void buildPage(const std::string& fileName, const std::string& contents);
 
 std::string tabText;
 std::string navTitle;
@@ -28,7 +27,7 @@ int main(int argc, char* argv[])
 {
 	if (std::string(argv[1]) == "init")
 	{
-		generateProjectFolders();
+		generateProjectFiles();
 		return 0;
 	}
 
@@ -42,7 +41,7 @@ int main(int argc, char* argv[])
 	return 1;
 }
 
-void generateProjectFolders()
+void generateProjectFiles()
 {
 	fs::createDirectory("src");
 	fs::createDirectory("build");
@@ -53,38 +52,35 @@ void build()
 {
 	loadConfig();
 	clearBuildDir();
+	generateNavCode();
 
-	navCode = generateNavCode();
-
-	std::string srcDir = fs::combinePaths(fs::getCwd(), "src");
-	std::string buildDir = fs::combinePaths(fs::getCwd(), "build");
-
-	for (const auto& file : std::filesystem::directory_iterator(srcDir))
+	for (const auto& file : std::filesystem::directory_iterator("src"))
 	{
 		if (file.path().extension() == ".txt")
 		{
 			std::string contents = fs::readFile(file.path().string());
 			strex::replace(contents, "\n", "<br>");
-			contents = buildPage(contents);
-
-			fs::writeFile(fs::combinePaths(buildDir, file.path().stem().string() + ".html"), contents);
+			buildPage(file.path().stem().string() + ".html", contents);
 		}
 		else if (file.path().extension() == ".md")
 		{
 			std::stringstream markdownInput(fs::readFile(file.path().string()));
 			std::shared_ptr<maddy::Parser> parser = std::make_shared<maddy::Parser>();
 			std::string htmlOutput = parser->Parse(markdownInput);
-
-			fs::writeFile(fs::combinePaths(buildDir, file.path().stem().string() + ".html"), buildPage(htmlOutput));
+			buildPage(file.path().stem().string() + ".html", htmlOutput);
 		}
-		else
+		else if (file.path().extension() == ".png" || file.path().extension() == ".jpg")
 		{
-			fs::copy(file.path().string(), fs::combinePaths(buildDir, file.path().stem().string() + file.path().extension().string()));
+			std::string htmlImage = "<img src=\"" + file.path().filename().string() + "\">";
+			buildPage(file.path().filename().string() + ".html", htmlImage);
+
+			fs::copy(fs::combinePaths("src", file.path().filename().string()),
+				fs::combinePaths("build", file.path().filename().string()));
 		}
 	}
 
-	fs::writeFile(fs::combinePaths(buildDir, "style.css"), CSS_TEMPLATE);
-	generateIndexFile();
+	fs::writeFile(fs::combinePaths("build", "style.css"), CSS_TEMPLATE);
+	buildPage("index.html", "");
 }
 
 void loadConfig()
@@ -119,53 +115,50 @@ std::string configGetLineValue(const std::vector<std::string>& lines, int index)
 
 void clearBuildDir()
 {
-	std::string buildDir = fs::combinePaths(fs::getCwd(), "build");
-
-	for (const auto& path : std::filesystem::directory_iterator(buildDir))
+	for (const auto& path : std::filesystem::directory_iterator("build"))
 	{
 		std::filesystem::remove_all(path);
 	}
 }
 
-std::string generateNavCode()
+void generateNavCode()
 {
-	std::string result = "<table>\n";
+	navCode = "<table>\n";
 
-	std::string srcDir = fs::combinePaths(fs::getCwd(), "src");
-
-	for (const auto& file : std::filesystem::directory_iterator(srcDir))
+	for (const auto& file : std::filesystem::directory_iterator("src"))
 	{
 		std::string row;
-		std::string newName;
 
-		if (file.path().extension() == ".txt" || file.path().extension() == ".md")
-			newName = file.path().stem().string() + ".html";
-		else
-			newName = file.path().filename().string();
+		std::string displayName = file.path().filename().string();
+		strex::replace(displayName, ".txt", "");
+		strex::replace(displayName, ".md", "");
 
-		row = "<tr><td><a href=\"" + newName + "\">" + file.path().stem().string()
+		std::string name = displayName + ".html";
+
+		row = "<tr><td><a href=\"" + name + "\">" + displayName
 			+ "</a></td></tr>\n";
 
-		result += row;
+		navCode += row;
 	}
 
-	result += "</table>";
-
-	return result;
+	navCode += "</table>";
 }
 
-std::string buildPage(const std::string& contents)
+void buildPage(const std::string& fileName, const std::string& contents)
 {
-	std::string result = PAGE_TEMPLATE;
-	strex::replace(result, "[tab text]", tabText);
-	strex::replace(result, "[nav title]", navTitle);
-	strex::replace(result, "[nav content]", navCode);
-	strex::replace(result, "[main content]", contents);
-	return result;
-}
+	std::string navCode = ::navCode;
 
-void generateIndexFile()
-{
-	std::string buildDir = fs::combinePaths(fs::getCwd(), "build");
-	fs::writeFile(fs::combinePaths(buildDir, "index.html"), buildPage(""));
+	std::string displayName = fileName;
+	strex::replace(displayName, ".html", "");
+
+	strex::replace(navCode, "<tr><td><a href=\"" + fileName + "\">" + displayName + "</a></td></tr>",
+		"<tr id=\"selected\"><td><a href=\"" + fileName + "\">" + displayName + "</a></td></tr>");
+
+	std::string resultContent = PAGE_TEMPLATE;
+	strex::replace(resultContent, "[tab text]", tabText);
+	strex::replace(resultContent, "[nav title]", navTitle);
+	strex::replace(resultContent, "[nav content]", navCode);
+	strex::replace(resultContent, "[main content]", contents);
+
+	fs::writeFile(fs::combinePaths("build", fileName), resultContent);
 }
